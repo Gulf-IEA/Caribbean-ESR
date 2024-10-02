7b2aed80dfc53626ee497959020a34cf8e2edfb6
CarissaGervasi-NOAA <carissa.gervasi@noaa.gov>
2024-10-02 16:17:24 -0400

    updating the indicator matrix

--- indicator_processing/CreateIndicatorMatrix.R
+++ indicator_processing/CreateIndicatorMatrix.R
@@ -1,6 +1,6 @@
 # Script to pull all the indicators from the indicator_objects folder and merge the data into a matrix that can be used for stoplight plots.
 
-# I am working on turning this into a function and including the indicators with monthly data. For the monthly indicators we need to just average those into annual time steps.
+# For the monthly indicators we need to just average those into annual time steps. (Not yet included)
 
 
 rm(list = ls())
@@ -36,7 +36,69 @@ rdata_files <- list.files(indicator_dir, pattern = "\\.RData$", full.names = TRU
 all_data <- lapply(rdata_files, extract_data)
 all_data <- Filter(Negate(is.null), all_data)
 
-# Extract the datelist vectors and keep only those that are of class integer
+
+
+# Before we go any further, we need to pull out the indicators with monthly data and average them into annual indicators
+
+# the monthly indicators include Carib_Chl, Carib_SST, DegreeHeatingWeeks, OA, turbidity, and unemployment
+
+# Convert monthly data to annual by averaging values per year
+convert_to_annual <- function(data_list, date_format) {
+  # Extract the date and indicator values
+  name <- data_list$file_name
+  dates <- data_list$datelist
+  values <- data_list$indicators
+  
+  
+  # Parse the date according to the specified format
+  if (date_format == "%m-%Y") {
+    years <- as.integer(format(as.Date(paste0("01-", dates), "%d-%m-%Y"), "%Y"))
+  } else if (date_format == "%Y%m") {
+    years <- as.integer(substr(dates, 1, 4))  # Extract the first 4 characters as year
+  } else if (date_format == "%b%Y") {
+    years <- as.integer(format(as.Date(paste0("01", dates), "%d%b%Y"), "%Y"))
+  } else {
+    stop("Unsupported date format")
+  }
+  
+  # Create a dataframe for easier manipulation
+  df <- data.frame(year = years, values)
+  
+  # Group by year and calculate the annual average
+  annual_data <- aggregate(. ~ year, data = df, FUN = mean)
+  
+  # Update the datelist and indicators with the annual data
+  data_list$datelist <- annual_data$year
+  data_list$indicators <- as.data.frame(annual_data[, -1]) 
+  
+  return(data_list)
+}
+
+# Apply the function to each monthly dataset
+carib_Chl <- convert_to_annual(all_data[[3]], "%m-%Y")
+Carib_SST <- convert_to_annual(all_data[[4]], "%m-%Y")
+DegreeHeatingWeeks <- convert_to_annual(all_data[[7]], "%Y%m")
+OA <- convert_to_annual(all_data[[18]], "%b%Y")
+turbidity <- convert_to_annual(all_data[[38]], "%m-%Y")
+unemployment <- convert_to_annual(all_data[[39]], "%Y%m")
+
+
+# Ok now we have all the monthly indicators as annual indicators. We need to replace these in the all_data list. 
+
+all_data[[3]] = carib_Chl
+all_data[[4]] = Carib_SST
+all_data[[7]] = DegreeHeatingWeeks
+all_data[[18]] = OA
+all_data[[38]] = turbidity
+all_data[[39]] = unemployment
+
+
+
+########################################################
+
+#Now back to making the indicator matrix
+
+# Extract the datelist vectors and keep only those that are of class integer (this isn't really necessary now that we've taken the average of the monthly data, but can keep if we want to ignore the monthly data and skip the above code chunk)
 datelists <- lapply(all_data, function(x) x$datelist)
 datelists <- datelists[sapply(datelists, function(d) class(d) == "integer")]
 
@@ -46,6 +108,8 @@ year_range <- seq(min(years, na.rm = TRUE), max(years, na.rm = TRUE))
 
 # Create an empty matrix with "year" column
 matrix_data <- data.frame(year = year_range)
+
+
 
 # Function to add indicator columns to the matrix
 add_indicator_columns <- function(matrix_data, data, descriptive_names) {
@@ -81,7 +145,26 @@ for (data in all_data) {
 # Print the resulting matrix
 print(matrix_data)
 
+# re-order the columns
+
+# Get the current column names of the matrix (excluding the first one)
+current_colnames <- colnames(matrix_data)[-1]
+
+# Match the current column names with the "desc_name" in the descriptive_names data
+reorder_index <- match(current_colnames, descriptive_names$desc_name)
+
+# Check for any unmatched columns
+if (any(is.na(reorder_index))) {
+  print("Warning: Some columns in the matrix do not match the CSV 'desc_name'.")
+}
+
+# Remove NA values from reorder_index and sort according to the 'order' column
+valid_columns <- !is.na(reorder_index)
+sorted_columns <- current_colnames[valid_columns][order(descriptive_names$order[reorder_index[valid_columns]])]
+
+# Reorganize the matrix, keeping the first column unchanged
+matrix_data <- matrix_data[, c(1, match(sorted_columns, colnames(matrix_data)))]
+
 # Save the matrix_data to an .rda file
 save(matrix_data, file = "indicator_data/all_indicators_matrix.rda")
-
 
