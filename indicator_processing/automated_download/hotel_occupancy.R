28e2af6483b93fc3e8fdc9a45ef1271a789fbb94
Mandy Karnauskas <Mandy.Karnauskas@nmfs.local>
2024-10-04 15:09:46 -0400

    adding multivariate analysis

--- indicator_processing/automated_download/hotel_occupancy.R
+++ indicator_processing/automated_download/hotel_occupancy.R
@@ -1,8 +1,74 @@
 
 rm(list = ls())
 
+install.packages("pdftools")
+library(pdftools)
+
+load("indicator_data/hotel_occupancy_rates_USVI_and_PR.RData")
+
+dates <- inddata$datelist
+dat <- inddata$indicators
+labs <- inddata$labels
+
+d2021 <- c(55, 65.4, 77.1, 51.5, 83.9, 79.2, 47, 62, 58.6, 52.4, 63.8,  65)
+d2022 <- c(49.2, 74.3, 72.3, 68.5, 66, 66.8, 65.9, 62.1, 44.6, 44, 55.9, 64)
+d2023 <- c(64.9, 63.2, 71.0, 61.5, 60.8, 61.5, 61.0, 52.2, 41.1, 43.1, 54.7, 55.5)
+
+mos <- rep(month.abb, 3)
+yrs <- c(rep(2021, 12), rep(2022, 12), rep(2023, 12))
+paste0(yrs, mos)
+
+
+
+
+url <- "https://usviber.org/wp-content/uploads/2024/07/H24-MAY.pdf"
+url <- "https://usviber.org/wp-content/uploads/2023/05/H22-DEC-revised-1.pdf"
+
+pdf <- pdf_text(url) %>% readr::read_lines()  
+
+pdf.labs <- pdf[c(grep("Period", pdf))]
+all_stat_lines <- pdf.labs %>% str_squish() %>% strsplit(split = " ")
+df1 <- plyr::ldply(all_stat_lines, header = T)
+
+st <- grep("January", pdf)[2]
+pdf.stats <- pdf[c(st:(st+11))]
+all_stat_lines <- pdf.stats %>% str_squish() %>% strsplit(split = " ")
+df <- plyr::ldply(all_stat_lines, header = T)
+names(df) <- df1
+
+
+
+
+
+
+[7] "                                ST. THOMAS/ST. JOHN                              ST. CROIX                      U.S.V.I., TOTAL"                  
+[8] "           Period"                                                                                                                                
+[9] "                                 2021          2022        % chg.         2021        2022      % chg.        2021       2022      % chg."        
+[10] "YEAR TO DATE:"                                                                                                                                    
+[11] "DECEMBER                          66.7          67.2           0.5        54.2         46.5        -7.7       63.2       60.9         -2.3"       
+[12] ""                                                                                                                                                 
+[13] "MONTH:"                                                                                                                                           
+[14] "January                           58.8          52.8          -5.9        44.9         39.9        -5.0       55.0        49.2        -5.8"       
+[15] "February                          69.9          79.1           9.3        52.9         62.0         9.1       65.4        74.3         8.9"       
+[16] "March                             82.9          76.8          -6.1        65.4         60.5        -4.9       77.1        72.3        -4.8"       
+[17] "April                             51.4          71.3          19.9        51.7         61.2         9.6       51.5        68.5       17.0"        
+[18] "May                               85.0          72.5         -12.5        80.0         50.0       -30.1       83.9        66.0       -17.9"       
+[19] "June                              81.7          73.6          -8.1        73.5         49.9       -23.6       79.2        66.8       -12.5"       
+[20] "July                              41.4          74.7          33.4        61.7         46.9       -14.8       47.0        65.9       19.0"        
+[21] "August                            64.8          70.6           5.8        54.3         44.2       -10.2       62.0        62.1         0.2"       
+[22] "September                         63.9          48.3         -15.6        44.5         36.8        -7.7       58.6        44.6       -14.0"       
+[23] "October                           59.8          47.8         -12.0        34.3         36.2         1.9       52.4        44.0        -8.3"       
+[24] "November                          71.9          66.7          -5.2        44.3         33.5       -10.8       63.8        55.9        -7.9"       
+[25] "December                          72.8          72.7          -0.1        46.4         46.0        -0.5       65.0        64.0         -1.0"       
+
+
+
+
+
+
 # https://rstudio-pubs-static.s3.amazonaws.com/415060_553527fd13ed4f30aae0f1e4483aa970.html
 # https://stackoverflow.com/questions/3746256/extract-links-from-webpage-using-r
+
 
 library(pdftools)
 library(xml2)
@@ -11,14 +77,38 @@ library(tidyverse)
 library(stringr)
 library(purrr)
 
+
 URL <- "https://usviber.org/archived-data/"
 
 page <- read_html(URL)
+
+
+
+# selecting the "a" HTML element storing the product URL 
+a_element <- html_product %>% html_element("a") 
+# selecting the "img" HTML element storing the product image 
+img_element <- html_product %>% html_element("img") 
+# selecting the "h2" HTML element storing the product name 
+h2_element <- html_product %>% html_element("h2") 
+# selecting the "span" HTML element storing the product price 
+span_element <- html_product %>% html_element("span")
   
 raw_list <- page %>% # takes the page above for which we've read the html
-html_nodes("a") %>%  # find all links in the page
-html_attr("href") %>% # get the url for these links
-str_subset("\\.pdf") %>% # find those that end in pdf only
+  html_nodes("a") %>%  # find all links in the page
+  html_attr("href") 
+
+%>% # get the url for these links
+  str_subset("\\.pdf")
+
+%>% # find those that end in pdf only
+  str_c("https://rbi.org.in", .) %>% # prepend the website to the url
+  map(read_html) %>% # take previously generated list of urls and read them
+  map(html_node, "#raw-url") %>% # parse out the 'raw' url - the link for the download button
+  map(html_attr, "href") %>% # return the set of raw urls for the download buttons
+  str_c("https://www.rbi.org.in", .) %>% # prepend the website again to get a full url
+  for (url in raw_list)
+  { download.file(url, destfile = basename(url), mode = "wb") 
+  } 
   
 
 pg <- read_html(URL)
---
+++ multivariate_summary.R
@@ -0,0 +1,123 @@
+
+
+rm(list = ls())
+library(plotTimeSeries)
+
+load("../Caribbean-ESR/indicator_data/all_indicators_matrix.rda")
+matrix_data
+
+class(matrix_data$year)
+matrix_data$year <- as.numeric(matrix_data$year)
+
+trafficLightPlot(matrix_data, noNAs = 0.99)
+# 2000+ has some data, 2010+ is data-rich period
+
+par(mfrow = c(7, 5))
+for (i in 1:ncol(matrix_data))  { hist(matrix_data[, i], main = names(matrix_data)[i]) }
+
+dev.off()
+
+d <- matrix_data[which(matrix_data$year >= 2000 & matrix_data$year <= 2023), ]
+trafficLightPlot(d, noNAs = 1)
+
+head(d)
+tail(d)
+
+risks <- c("DHW_PR", "DHW_VI", "OA", "ACE", "TURB_PR", "TURB_STT", "TURB_STX", "SST_MEAN", "SST_MIN", "SST_MAX", 
+           "POLL_PR", "POLL_VI", "CHL", "ETQ", "DISTB_PR", "DISTB_STT", "DISTB_STX", "SARG", "CRU_PR", "CRU_VI", "POP_PR", "POP_VI")
+food_fi <- c("VETU_PR", "GUTT_PR", "ANAL_PR", "CHRY_PR", "AURO_PR", "VIRI_PR", "VETU_STT", "GUTT_STT", "ANAL_STT", "CHRY_STT", 
+             "AURO_STT", "VIRI_STT", "VETU_STX", "GUTT_STX", "ANAL_STX", "CHRY_STX", "AURO_STX", "VIRI_STX", "DEN_PR", "DEN_VI", "SLPSIZ_VI")
+food_fd <- c("PDR_PR", "PDR_STT", "PDR_STX", "MLMAX_PR", "MLMAX_STT", "MLMAX_STX", 
+            "LOB_PR", "LOB_STT", "LOB_STX", "CONC_PR", "CONC_STT", "CONC_STX", "OTHR_PR", "OTHR_STT", "OTHR_STX")
+soc <- c("O_GDP_PR", "O_GDP_VI", "EST_PR", "EST_VI", "EMP_PR", "EMP_VI", "WAG_PR", "WAG_VI", "GDP_PR", "GDP_VI", 
+         "UNEM_PR", "UNEM_VI", "GINI_R_PR", "GINI_R_STT", "GINI_R_STX")
+oth <- c("REC_PR", "REC_VI", "DIV_PR", "DIV_STT", "DIV_STX", "BYCAT_PR", "BYCAT_STT", "BYCAT_STX", "REGS", "MGMT", 
+         "TR3_PR", "TR3_VI", "OUT_PR", "OUT_VI", "CRLRCH_PR", "CRLCVR_PR", "CRLRCH_VI", "CRLCVR_VI")
+
+d_risk <- d[, c(1, which(names(d) %in% risks))]
+d_foodfi <- d[, c(1, which(names(d) %in% food_fi))]
+d_foodfd <- d[, c(1, which(names(d) %in% food_fd))]
+d_soc <- d[, c(1, which(names(d) %in% soc))]
+d_oth <- d[, c(1, which(names(d) %in% oth))]
+
+dim(d_risk)
+dim(d_foodfi)
+dim(d_foodfd)
+dim(d_soc)
+dim(d_oth)
+
+par(mar = c(0.5, 10, 0, 1), mfrow = c(5, 1))
+
+trafficLightPlot(d_risk, noNAs = 1)
+trafficLightPlot(d_foodfi, noNAs = 1)
+trafficLightPlot(d_foodfd, noNAs = 1)
+trafficLightPlot(d_soc, noNAs = 1)
+trafficLightPlot(d_oth, noNAs = 1)
+
+# alternative categorization
+
+risks <- c("DHW_PR", "DHW_VI", "OA", "ACE", "TURB_PR", "TURB_STT", "TURB_STX", "SST_MEAN", "SST_MIN", "SST_MAX", 
+           "POLL_PR", "POLL_VI", "CHL", "ETQ", "DISTB_PR", "DISTB_STT", "DISTB_STX", "SARG", "CRU_PR", "CRU_VI", "POP_PR", "POP_VI")
+fi <- c("VETU_PR", "GUTT_PR", "ANAL_PR", "CHRY_PR", "AURO_PR", "VIRI_PR", "VETU_STT", "GUTT_STT", "ANAL_STT", "CHRY_STT", 
+            "AURO_STT", "VIRI_STT", "VETU_STX", "GUTT_STX", "ANAL_STX", "CHRY_STX", "AURO_STX", "VIRI_STX", 
+        "DEN_PR", "DEN_VI", "SLPSIZ_VI")
+ccl <- c("PDR_PR", "PDR_STT", "PDR_STX", "MLMAX_PR", "MLMAX_STT", "MLMAX_STX", 
+         "LOB_PR", "LOB_STT", "LOB_STX", "CONC_PR", "CONC_STT", "CONC_STX", "OTHR_PR", "OTHR_STT", "OTHR_STX", 
+         "GINI_R_PR", "GINI_R_STT", "GINI_R_STX", 
+         "DIV_PR", "DIV_STT", "DIV_STX", "BYCAT_PR", "BYCAT_STT", "BYCAT_STX")
+oth <- c("EST_PR", "EST_VI", "EMP_PR", "EMP_VI", "WAG_PR", "WAG_VI", "GDP_PR", "GDP_VI", "UNEM_PR", "UNEM_VI",
+         "REC_PR", "REC_VI", "REGS", "MGMT", "TR3_PR", "TR3_VI", "OUT_PR", "OUT_VI", "CRLRCH_PR", "CRLCVR_PR", "CRLRCH_VI", "CRLCVR_VI")
+
+d_risk <- d[, c(1, which(names(d) %in% risks))]
+d_fi <- d[, c(1, which(names(d) %in% fi))]
+d_ccl <- d[, c(1, which(names(d) %in% ccl))]
+d_oth <- d[, c(1, which(names(d) %in% oth))]
+
+# traffic light plots ----------------------------------
+
+png(filename = "traffic.png", units="in", width=3.8, height=11, pointsize=12, res=72*12)
+
+par(mar = c(1.9, 8, 0.1, 1), mfrow = c(4, 1))
+
+trafficLightPlot(d_risk, noNAs = 1, cexlabs = 0.8)
+mtext(side = 2, "risks to meeting objectives", line = 6.5, cex = 0.9)
+trafficLightPlot(d_fi, noNAs = 1, cexlabs = 0.8)
+mtext(side = 2, "fishery-independent indicators", line = 6.5, cex = 0.9)
+trafficLightPlot(d_ccl, noNAs = 1, cexlabs = 0.8)
+mtext(side = 2, "fishery-dependent indicators", line = 6.5, cex = 0.9)
+trafficLightPlot(d_oth, noNAs = 1, cexlabs = 0.8)
+mtext(side = 2, "other management indicators", line = 6.5, cex = 0.9)
+
+dev.off()
+
+# ordination plots ----------------------------
+
+thresh <- 0.1
+minyr <- 2011
+
+png(filename = "pcas.png", units="in", width=8, height=11, pointsize=12, res=72*12)
+
+par(mar = c(4, 6, 1, 1), mfrow = c(3, 2))
+
+pc <- plotOrdScores(d_risk, main = "", mintime = 2011, noNAs = thresh) #, method = "nmds", nmdsk = 3)
+mtext(side = 2, "risks to meeting objectives", line = 4.5, cex = 0.8)
+barplot(pc$rotation[,1], las = 2, horiz = T, xlim = c(-0.45, 0.45))
+abline(v = c(-0.2, 0.2), col = 8, lty = 2)
+
+#pc <- plotOrdScores(d_fi, main = "fishery-independent indicators", mintime = 2011, noNAs = thresh) #, method = "nmds", nmdsk = 3)
+#barplot(pc$rotation[,1], las = 2, horiz = T)
+#abline(v = c(-0.2, 0.2), col = 8, lty = 2)
+
+pc <- plotOrdScores(d_ccl, main = "", mintime = 2011, noNAs = thresh) #, method = "nmds", nmdsk = 3)
+mtext(side = 2, "fishery-dependent indicators", line = 4.5, cex = 0.8)
+barplot(pc$rotation[,1], las = 2, horiz = T, xlim = c(-0.45, 0.45))
+abline(v = c(-0.2, 0.2), col = 8, lty = 2)
+
+pc <- plotOrdScores(d_oth, main = "", mintime = 2011, noNAs = thresh) #, method = "nmds", nmdsk = 3)
+mtext(side = 2, "other management indicators", line = 4.5, cex = 0.8)
+barplot(pc$rotation[,1], las = 2, horiz = T, xlim = c(-0.45, 0.45))
+abline(v = c(-0.2, 0.2), col = 8, lty = 2)
+
+dev.off()
+
+
